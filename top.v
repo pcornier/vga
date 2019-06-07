a0b56704ea495dd807e9fc1009d35f71ac99ba20
Pierre CORNIER <pier.cornier@gmail.com>
2019-06-07 15:15:57 +0200

    first commit

---
+++ top.v
@@ -0,0 +1,30 @@
+
+module top(
+
+  input clk,
+
+  output [4:0] red,
+  output [5:0] green,
+  output [4:0] blue,
+  output h_sync,
+  output v_sync
+
+);
+
+wire [9:0] xpos;
+wire [9:0] ypos;
+
+// three small boxes on the screen
+assign red = xpos > 50 && xpos < 90 && ypos > 50 && ypos < 90 ? 5'b11111 : 5'b0;
+assign green = xpos > 65 && xpos < 105 && ypos > 65 && ypos < 105 ? 6'b111111 : 6'b0;
+assign blue = xpos > 80 && xpos < 120 && ypos > 80 && ypos < 120 ? 5'b11111 : 5'b0;
+
+vga display(
+  .clk(clk),
+  .h_sync(h_sync),
+  .v_sync(v_sync),
+  .xpos(xpos),
+  .ypos(ypos)
+);
+
+endmodule
---
+++ vga.v
@@ -0,0 +1,58 @@
+
+// 640x480 = 800x524 = 419200 pixels
+// 419200 x 60 f/s = 25 152 000 px/s = ~25mhz
+//
+//    | video | front porch | sync pulse | back porch |
+// ---+-------+-------------+------------+------------|
+// hz |   640 |          16 |         96 |         48 | = 800
+// ---+-------+-------------+------------+------------|
+// vt |   480 |          11 |          2 |         31 | = 524
+// ---+-------+-------------+------------+------------|
+//
+// hz events: V:[0]-639 FP:[640]-655 SP:[656]-751 BP:[752]-800
+// vt events: V:[0]-479 FP:[480]-490 SP:[491]-492 BP:[493]-524
+
+module vga(
+  input clk,
+  output wire h_sync,
+  output wire v_sync,
+  output [9:0] xpos,
+  output [9:0] ypos
+);
+
+reg [9:0] h_count;
+reg [9:0] v_count;
+
+// 25M clock
+reg clk_en;
+initial clk_en = 1'b0;
+always @(posedge clk)
+  clk_en <= ~clk_en;
+
+parameter
+  HFP = 640,
+  VFP = 480,
+  HSP = 656,
+  VSP = 491,
+  HBP = 752,
+  VBP = 493,
+  HPX = 800,
+  VPX = 524;
+
+assign h_sync = ~((h_count >= HSP) && (h_count < HBP));
+assign v_sync = ~((v_count >= VSP) && (v_count < VBP));
+assign xpos = h_count < HFP ? h_count : 10'b0;
+assign ypos = v_count < VFP ? v_count : 10'b0;
+
+always @(posedge clk_en) begin
+  if (h_count == HPX) begin
+    h_count <= 0;
+    v_count <= v_count + 1;
+  end
+  else
+    h_count <= h_count + 1;
+  if (v_count == VPX)
+    v_count <= 0;
+end
+
+endmodule
